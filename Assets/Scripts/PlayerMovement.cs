fd78243627d00adc3a29da937dfccf5e1f6fd284
dmccowan <mccowa206@morrisville.edu>
2025-10-29 15:31:48 -0400

    Added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float controlSpeed = 10f;
+    Vector2 movement;
     void Start()
     {
-        
     }
     void Update()
     {
-
+        ProcessTranslation();
     }
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        movement = value.Get<Vector2>();
+    }
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
     }
 }
