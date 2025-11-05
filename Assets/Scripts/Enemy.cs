4a880a2859b1e51eb8cd7646bafee02ab93c182c
dmccowan <mccowa206@morrisville.edu>
2025-11-05 14:39:53 -0500

    Runtime instantiation implemented

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject destroyedFX;
     void OnParticleCollision(GameObject other)
     {
+        Instantiate(destroyedFX, this.transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
 }
