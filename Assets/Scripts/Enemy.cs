402e28e7355b85353fe4a0bf3c74d7bab066fb98
dmccowan <mccowa206@morrisville.edu>
2025-11-06 07:22:28 -0500

    Added hitpoints with custom values to enemy variants

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedFX;
+    [SerializeField] int hitPoints = 3;
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedFX, this.transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+        if (hitPoints <= 0)
+        {
+            Instantiate(destroyedFX, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
